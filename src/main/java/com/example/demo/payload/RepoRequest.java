5acd258be9ebba60a339704bd961a5beb3561eb6
Chandan <ckumar@xebia.com>
2018-03-15 17:21:41 +0530

    Github client repositories

--- src/main/java/com/example/demo/payload/RepoRequest.java
+++ src/main/java/com/example/demo/payload/RepoRequest.java
@@ -2,10 +2,12 @@ package com.example.demo.payload;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@Data
 public class RepoRequest {
     @NotBlank
     private String name;
